bc6f629a2364186a96ee6e2d09b71aee3c0964b8
Gonzalo <gonzalomedinamedina@gmail.com>
2024-01-21 13:34:06 +0100

    Created AgeService to calculate Age with accuracy from years to seconds.

--- Q3/AgeCalculator/AgeCalculator.Server/Services/AgeService.cs
+++ Q3/AgeCalculator/AgeCalculator.Server/Services/AgeService.cs
@@ -1,6 +1,52 @@
-﻿namespace AgeCalculator.Server.Services
+﻿using AgeCalculator.Server.Entities;
+
+namespace AgeCalculator.Server.Services
 {
+	/// <summary>
+	/// Implementation of the IAgeService
+	/// </summary>
 	public class AgeService : IAgeService
 	{
+		public Age calculateAge(DateTime birthday, DateTime today)
+		{
+			int years = today.Year - birthday.Year,
+				months = today.Month - birthday.Month,
+				days = today.Day - birthday.Day,
+				hours = today.Hour - birthday.Hour,
+				minutes = today.Minute - birthday.Minute,
+				seconds = today.Second - birthday.Second;
+
+			if (seconds < 0)
+			{
+				minutes--;
+				seconds += 60;
+			}
+
+			if (minutes < 0)
+			{
+				hours--;
+				minutes += 60;
+			}
+
+			if (hours < 0)
+			{
+				days--;
+				hours += 24;
+			}
+
+			if (days < 0)
+			{
+				months--;
+				days += DateTime.DaysInMonth(birthday.Year, birthday.Month);
+			}
+
+			if (months < 0)
+			{
+				years--;
+				months += 12;
+			}
+
+			return new Age(years, months, days, hours, minutes, seconds);
+		}
 	}
 }
--- Q3/AgeCalculator/AgeCalculator.Server/Services/IAgeService.cs
+++ Q3/AgeCalculator/AgeCalculator.Server/Services/IAgeService.cs
@@ -1,6 +1,18 @@
-﻿namespace AgeCalculator.Server.Services
+﻿using AgeCalculator.Server.Entities;
+
+namespace AgeCalculator.Server.Services
 {
+	/// <summary>
+	/// Interface that manage Age operations like calculate exact Age based on a birthday DateTime stamp.
+	/// </summary>
 	public interface IAgeService
 	{
+		/// <summary>
+		/// Return an Age object that has Age information.
+		/// </summary>
+		/// <param name="birthday">The birthday of the person in DateTime format</param>
+		/// <param name="todaysDate">The todays date time.</param>
+		/// <returns>Age object with information about the Age calculated by the birthday parameter.</returns>
+		Age calculateAge(DateTime birthday, DateTime todaysDate);
 	}
 }
