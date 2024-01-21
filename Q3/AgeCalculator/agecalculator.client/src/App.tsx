21cb9648bfa62bf9bdada448769bb286739b0a08
Gonzalo <gonzalomedinamedina@gmail.com>
2024-01-21 13:28:07 +0100

    Specifing types in DateSelector component. Changing date input format. Created component for age result. Specifing types in fetchAge function and removing try catch and adding catch to fetch statement. Added alert for non valid input dates.

--- Q3/AgeCalculator/agecalculator.client/src/App.tsx
+++ Q3/AgeCalculator/agecalculator.client/src/App.tsx
@@ -3,9 +3,9 @@ import './App.css';
 import DateSelector from './Components/DateSelector';
 
 function App() {
-    const [age, setAge] = useState('');
+    const [age, setAge] = useState(undefined);
 
-    const getFormatDate = (date) =>
+    const getFormatDate = (date: Date) =>
     {
         let day = String(date.getDate()).padStart(2, '0');
         let month = String(date.getMonth() + 1).padStart(2, '0');
@@ -15,30 +15,38 @@ function App() {
     }
 
 
-    const fetchAge = async (event, date) =>
-    {        
+    const fetchAge = async (event: Event, birthday: Date) =>
+    {       
         event.preventDefault();
-        try
+        const birthdayObject = new Date(birthday);
+
+        if (birthdayObject > new Date())
         {
-            fetch('ageCalculator',
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(date)
-                })
-            .then(response => response.json())
-            .then(data =>
-            {
-                console.log("dataaa" + data);
-            })
-        }
-        catch (error)
+            alert("Introduce date values before todays date.")
+            return;
+        } 
+
+        fetch('ageCalculator',
         {
-            console.error(error);
-        }
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(birthdayObject)
+        })
+        .then(response => response.json())
+        .then(data =>
+        {
+            setAge(data);
+        })
+        .catch((error) => {
+            console.log(error)
+        })
     }
+
+    const ageComponent = age !== undefined ?
+        <p className="age">This is your exact age! {age.years} years, {age.months} months, {age.days} days, {age.hours} hours, {age.minutes} minutes, {age.seconds} seconds.</p> :
+        null;
 
     return (
 
@@ -47,8 +55,9 @@ function App() {
                 <h1>Age calculator</h1>
             </div>
             <div className="content">
+                {ageComponent}
                 <p>Today's date: {getFormatDate(new Date())}</p>
-                <DateSelector onSubmit={fetchAge} />
+                <DateSelector onSubmit={fetchAge}/>
             </div>
         </div>
     );
--- Q3/AgeCalculator/agecalculator.client/src/Components/DateSelector.tsx
+++ Q3/AgeCalculator/agecalculator.client/src/Components/DateSelector.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-const DateSelector = ({onSubmit}) =>
+const DateSelector = ({onSubmit}:any) =>
 {
     const [selectedDate, setSelectedDate] = useState('');
 
-    const handleDateChange = (event) => {
+    const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newDate = event.target.value;
         setSelectedDate(newDate);
     };
@@ -12,7 +12,7 @@ const DateSelector = ({onSubmit}) =>
     return <>
         <form onSubmit={(e) => onSubmit(e, selectedDate)}>
             <label>Birthday: </label>
-            <input type="date" id="birthday" name="birthday" value={selectedDate} onChange={handleDateChange} />
+            <input type="date" id="birthday" name="birthday" placeholder="dd-mm-yyyy" pattern="\d{2}-\d{2}-\d{4}" value={selectedDate} onChange={handleDateChange} />
             <input type="submit" />
         </form>
     </>
