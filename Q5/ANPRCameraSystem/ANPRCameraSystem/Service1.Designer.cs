1314bed85b5ba0b367ca934f4db4fc07a9ecb8eb
Gonzalo <gonzalomedinamedina@gmail.com>
2024-01-22 09:26:04 +0100

    Changed Service1 to read camera and store its file info into database,

--- Q5/ANPRCameraSystem/ANPRCameraSystem/Service1.Designer.cs
+++ Q5/ANPRCameraSystem/ANPRCameraSystem/Service1.Designer.cs
@@ -1,18 +1,16 @@
 ﻿using System;
+using System.ServiceProcess;
 
 namespace ANPRCameraSystem
 {
-	partial class Service1
+	public partial class Service1 : ServiceBase
 	{
 		/// <summary> 
 		/// Variable del diseñador necesaria.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		/// Limpiar los recursos que se estén usando.
-		/// </summary>
-		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+
 		protected override void Dispose(bool disposing)
 		{
 			if (disposing && (components != null))
@@ -24,13 +22,8 @@ namespace ANPRCameraSystem
 
 		#region Código generado por el Diseñador de componentes
 
-		/// <summary> 
-		/// Método necesario para admitir el Diseñador. No se puede modificar
-		/// el contenido de este método con el editor de código.
-		/// </summary>
 		private void InitializeComponent()
 		{
-			Console.Write("holaaa desde el service");
 			this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
 			// 
 			// elementHost1
@@ -45,7 +38,6 @@ namespace ANPRCameraSystem
 			// Service1
 			// 
 			this.ServiceName = "Service1";
-
 		}
 
 		#endregion
--- Q5/ANPRCameraSystem/ANPRCameraSystem/Service1.cs
+++ Q5/ANPRCameraSystem/ANPRCameraSystem/Service1.cs
@@ -1,9 +1,24 @@
-﻿using System.ServiceProcess;
+﻿using ANPRCameraSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.ServiceProcess;
+using System.Text.RegularExpressions;
+using System.Windows.Documents;
 
 namespace ANPRCameraSystem
 {
 	public partial class Service1 : ServiceBase
 	{
+		/// <summary>
+		/// Path where camera saves .lpr files
+		/// </summary>
+		private readonly string plateFileFolder = "C:\\Plates";
+		/// <summary>
+		/// Extension of the files to be found
+		/// </summary>
+		private readonly string extensionFile = "*.lpr";
+
 		public Service1()
 		{
 			InitializeComponent();
@@ -11,6 +26,37 @@ namespace ANPRCameraSystem
 
 		protected override void OnStart(string[] args)
 		{
+			System.Timers.Timer timer = new System.Timers.Timer();
+			timer.Elapsed += ReadCameraFile;
+			//This should be replaced by invoking the service when the camera triggers.
+			timer.Interval = 10000;
+		}
+
+		private void ReadCameraFile(object sender, System.Timers.ElapsedEventArgs e)
+		{
+			foreach (var filePath in Directory.GetFiles(plateFileFolder, extensionFile))
+			{
+				ReadFile(filePath);
+			}
+		}
+
+		private void ReadFile(string filePath)
+		{
+			string[] lines = File.ReadAllLines(filePath);
+			var columns = Regex.Split(lines[0], "[\\/]");
+
+			string countryOfVehicle = columns[0];
+			string regNumber = columns[1];
+			string confidenceLevel = columns[2];
+			string cameraName = columns[3];
+			DateTime date = DateTime.ParseExact(columns[4], "yyyyMMdd", null);
+			string time = columns[5];
+			string plateName = columns[6];
+
+			var vehiclePlate = new VehiclePlate( countryOfVehicle, regNumber, confidenceLevel, cameraName , date, time, plateName);
+			var dbService = new DbService.DbService();
+
+			dbService.AddEntity(vehiclePlate);
 		}
 
 		protected override void OnStop()
